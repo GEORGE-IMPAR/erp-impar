74a9d2373a97ac2314ae65acfa5cb1d1cc518d45
GEORGE-IMPAR <ramos.george@outlook.com>
2025-10-28 18:47:07 -0300

    consulta_json_v3_beauty_brand_fixclose_loader_OS.js

--- documentos/consulta_json_v3_beauty_brand_fixclose_loader_OS.js
+++ documentos/consulta_json_v3_beauty_brand_fixclose_loader_OS.js
@@ -181,7 +181,7 @@
 
       async function gerarOSOnce(c) {
         try {
-          const res = await fetch('/api/gerador/make_os.php?codigo=' + encodeURIComponent(c), { cache: 'no-store' });
+          const res = await fetch('/api/gerador/make_contract.php?codigo=' + encodeURIComponent(c), { cache: 'no-store' });
           const j = await res.json();
           if (j && j.ok && j.url) {
             window.open(j.url, '_blank');
@@ -404,6 +404,7 @@ window.__CJFIX_API__ = {
 };
 
 })();
+
 
 
 
