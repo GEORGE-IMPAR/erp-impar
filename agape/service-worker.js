5b5aa5ee23d08342e51b4c30644bd5b67b318309
GEORGE-IMPAR <ramos.george@outlook.com>
2026-02-05 16:55:05 -0300

    Update service worker to use PNG icon
    Replaced icon-192.gif with icon-192.png in the service worker cache.

--- agape/service-worker.js
+++ agape/service-worker.js
@@ -7,7 +7,7 @@ const CORE_ASSETS = [
   "./manifest.webmanifest",
   "./service-worker.js",
   "./favicon.ico",
-  "./assets/icon-192.gif",
+  "./assets/icon-192.png",
   "./assets/icon-512.gif",
   "./assets/logo-Clementino-Brito.jpeg",
   "./assets/logo-pix.jpg",
