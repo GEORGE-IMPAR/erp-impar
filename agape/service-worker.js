a807d73787ee31d64600f4cd4b036b1de6616e86
GEORGE-IMPAR <ramos.george@outlook.com>
2026-02-06 12:24:41 -0300

    Update service worker to use PNG icons
    Replaced GIF icons with PNG versions in the service worker.

--- agape/service-worker.js
+++ agape/service-worker.js
@@ -7,8 +7,8 @@ const CORE_ASSETS = [
   "./manifest.webmanifest",
   "./service-worker.js",
   "./favicon.ico",
-  "./assets/icon-192.gif",
-  "./assets/icon-512.gif",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
   "./assets/logo-Clementino-Brito.jpeg",
   "./assets/logo-pix.jpg",
 
